0430db48cf8380b2f49f3269555f03ef6e46897b
Aleksandr Kozlovets <kozlovetsalexandr@gmail.com>
2021-08-21 13:30:37 +0300

    Create pages folder, add basic index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,31 @@
 <template>
-  <div>Test</div>
+  <section class="homes">
+    <ul class="homes__list homes-list">
+      <li v-for="home in homes" :key="home.objectID" class="homes-list__item">
+        <HomeCard :home="home" />
+      </li>
+    </ul>
+  </section>
 </template>
+
+<script>
+import homes from '~/data/homes.json'
+export default {
+  data() {
+    return {
+      homes: homes.slice(0, 3)
+    }
+  },
+}
+</script>
+
+<style scoped>
+.homes-list {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 20px;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+</style>
